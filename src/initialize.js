c4ff2e9e8462cf93c89437012679a5031bebe49a
Jordan <jordansampson23@gmail.com>
2023-01-19 13:59:02 -0600

    Initialize module with header and nav create functions.

---
+++ src/initialize.js
@@ -0,0 +1,43 @@
+// import modules
+
+
+function createHeader() {
+    const header = document.createElement('header');
+    header.appendChild(createNav())
+    return header;
+}
+
+function createNav() {
+    const nav = document.createElement('nav');
+
+
+    const homeBtn = document.createElement('button');
+    homeBtn.textContent = 'Home'
+    homeBtn.addEventListener('click', (e) => {
+        // Handle active state
+        loadHome()
+    })
+    
+
+    const menuBtn = document.createElement('button');
+    menuBtn.textContent = 'Menu'
+    menuBtn.addEventListener('click', (e) => {
+        // Handle active state
+        loadMenu()
+    })
+
+
+    const contactBtn = document.createElement('button')
+    contactBtn.textContent = 'Contact'
+    contactBtn.addEventListener('click', (e) => {
+        // Handle active state
+        loadContact()
+    })
+
+    nav.appendChild(homeBtn);
+    nav.appendChild(menuBtn);
+    nav.appendChild(contactBtn);
+
+    return nav
+}
+

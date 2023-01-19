cebf310f92fcc1ba9d424f396c5c0abbc3c447d9
Jordan <jordansampson23@gmail.com>
2023-01-19 14:29:01 -0600

    Initialize home.js

---
+++ src/home.js
@@ -0,0 +1,24 @@
+function createHome() {
+    const home = document.createElement('div');
+    home.classList.add('wrapper')
+
+    home.appendChild(createTitle('Bikini Bottom Krabb Shack'))
+
+
+
+    return home;
+}
+
+function createTitle(text) {
+    const title = document.createElement('h1');
+    title.textContent = text
+    return title;
+}
+
+function loadHome() {
+    const main = document.getElementById('main');
+    main.textContent = "";
+    main.appendChild(createHome());
+}
+
+export default loadHome;

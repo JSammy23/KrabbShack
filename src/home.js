73f3eda70ed145f81bd14f2dd88adf5f1600a5a3
Jordan <jordansampson23@gmail.com>
2023-01-19 14:30:41 -0600

    Styling

--- src/home.js
+++ src/home.js
@@ -11,6 +11,7 @@ function createHome() {
 
 function createTitle(text) {
     const title = document.createElement('h1');
+    title.classList.add('title')
     title.textContent = text
     return title;
 }

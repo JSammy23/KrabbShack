4b3cbcb39c77170c9549ff683ceca0a7516a719c
Jordan <jordansampson23@gmail.com>
2023-01-20 13:25:24 -0600

    Menu module initiated

---
+++ src/menu.js
@@ -0,0 +1,16 @@
+function createMenu() {
+    const menu = document.createElement('div');
+    menu.classList.add('menu', 'wrapper');
+
+    menu.appendChild(createTitle('Krabb Shack Menu'));
+}
+
+function createTitle(text) {
+    const title = document.createElement('h1');
+    title.textContent = text
+}
+
+function createContent() [{
+    const newDiv = document.createElement('div');
+    
+}]

d8cfe6f5edea1e8576e76f0ceb03c7d9666cf554
Jordan <jordansampson23@gmail.com>
2023-01-21 10:02:15 -0600

    loadMenu fn export

--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,35 @@
+import Grub from './assets/menu.jpg';
+
 function createMenu() {
     const menu = document.createElement('div');
     menu.classList.add('menu', 'wrapper');
 
-    menu.appendChild(createTitle('Krabb Shack Menu'));
+    menu.appendChild(createTitle('Galley Grub'));
+    menu.appendChild(createImg());
+
+    return menu;
 }
 
 function createTitle(text) {
     const title = document.createElement('h1');
     title.textContent = text
+    return title;
 }
 
-function createContent() [{
+function createImg() {
+    const img = new Image
+    img.src = Grub
+    return img
+}
+
+function createContent() {
     const newDiv = document.createElement('div');
-    
-}]
+
+
+}
+
+export default function loadMenu() {
+    const main = document.getElementById('main');
+    main.textContent = "";
+    main.appendChild(createMenu());
+}
